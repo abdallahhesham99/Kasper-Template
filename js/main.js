6c865afd29308bf0cb921c4394509705d61a32c1
abdallahhesham99 <abdallahhesham414@gmail.com>
2022-06-17 09:40:22 +0200

    home section

--- js/main.js
+++ js/main.js
@@ -4,3 +4,17 @@ const navMenu = document.querySelector(".nav__links");
 toggleButton.addEventListener("click", function () {
   navMenu.classList.toggle("active");
 });
+// =====================================================================
+// ======================== Home section ===============================
+const swiper = new Swiper(".swiper", {
+  loop: false,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
